87d32ffe8631d04a9d72921aa2194b1e5ad4bffc
Oxram <mihail@ohramovich.ru>
2023-07-09 13:47:28 +0300

    first

---
+++ Homework/Homework 1/Program.cs
@@ -0,0 +1,98 @@
+﻿
+//Example 2
+/*Console.WriteLine("enter the first number");
+int a= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("enter the second number");
+int b= Convert.ToInt32(Console.ReadLine());
+
+
+ if(a>b){
+    Console.WriteLine("max="+a);
+ }
+ else{
+    Console.WriteLine("max="+b);
+ }
+*/
+//Example 4
+
+/*Console.WriteLine("enter the first number");
+int c= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("enter the second number");
+int d= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("enter the third number");
+int f= Convert.ToInt32(Console.ReadLine());
+
+
+ if(c>d)
+ {
+    Console.WriteLine("max="+c);
+ }
+ else{
+    if(d>f)
+    {
+    Console.WriteLine("max="+d);
+    }
+    else
+    {
+    Console.WriteLine("max="+f);
+    }
+ }
+*/
+//Example 6
+
+/*(Console.WriteLine("enter the first number");
+int g= Convert.ToInt32(Console.ReadLine());
+    if(g%2==0)
+    {
+    Console.WriteLine("The number is honest");
+    }    
+    else   
+    {
+    Console.WriteLine("The number is not honest");
+    }  
+
+ */
+
+//Example 8
+Console.WriteLine("enter the first number");
+int g= Convert.ToInt32(Console.ReadLine());
+int max;
+    if(g%2==0)
+    {
+        while(g>0)
+        {
+          Console.WriteLine("The number is not honest"+ g);  
+          g=g-2;
+        }
+    
+
+    }    
+    else   
+    {
+        g=g-1;
+        while(g>0)
+        {
+        
+        Console.WriteLine("The number is not honest "+(g));
+        g=g-2;
+        }
+    }  
+
+
+
+
+ 
+
+
+  
+ 
+
+
+
+
+
+
+ 
+
+
+
---
+++ Homework/Homework 2/Program.cs
@@ -0,0 +1,78 @@
+﻿//example 15
+/*
+while (true)
+{
+Console.WriteLine("Enter the First number!");
+int firstNumber=Convert.ToInt32(Console.ReadLine());
+    if(firstNumber>7)
+    {
+    Console.WriteLine("Incorrect data entry, enter a number from 1 to 7");
+    } 
+    else if(firstNumber<6)
+    { 
+    
+        Console.WriteLine("Working day");
+        break;
+    }
+    else
+    
+    {
+        Console.WriteLine("Weekand");
+        break;
+    }
+}
+*/
+
+//example 10
+/*
+while(true)
+    {
+        Console.WriteLine("enter a three-digit number");
+        int number=Convert.ToInt32(Console.ReadLine());
+        if(number<100)
+        {
+            Console.WriteLine("The number is not three-digit, enter a three-digit number");
+            number=Convert.ToInt32(Console.ReadLine());
+        }
+        else if(number>1000)
+        {
+            Console.WriteLine("The number is not three-digit, enter a three-digit number");
+            number=Convert.ToInt32(Console.ReadLine());
+        }
+        else
+        {
+            number=number/10%10;
+            Console.WriteLine(number);
+            break;
+        }
+
+    }
+    */
+//example 13
+
+//int thirdnumber(int a)_
+  
+        
+        Console.WriteLine("Enter number");
+        int number =Convert.ToInt32(Console.ReadLine());
+
+    if(number<100)
+        {
+        Console.WriteLine("There is no third digit");
+        }
+        else
+        {
+           while(number>1000)
+           {
+            number=number/10;
+           }
+            number=number%10;
+            Console.WriteLine(number);
+            
+        }
+        
+    
+
+    
+
+    
